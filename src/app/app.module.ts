ade7bc42bf3ef791f25695426f31b50e7e0bb31a
petrovicmilos <milosp1995@gmail.com>
2023-10-17 08:39:13 +0200

    commit after adding popular component

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AvailableFlightsComponent } from './available-flights/available-flights.component';
+import { BannerComponent } from './banner/banner.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { FlightsListComponent } from './flights-list/flights-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { PopularComponent } from './popular/popular.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AvailableFlightsComponent,
+    BannerComponent,
+    FlightsListComponent,
+    FooterComponent,
+    PopularComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/popular/popular.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-popular',
+  templateUrl: './popular.component.html',
+  styleUrls: ['./popular.component.css'],
+  providers: [
+    provideAnimations(),
+  ],
+  animations: [
+    // animation triggers go here
+  ]
+})
+export class PopularComponent {
+
+  cards = [
+    {
+      image: '../assets/madrid.jpg',
+      title: 'Madrid',
+      price: 'From $30'
+    },
+    {
+      image: '../assets/paris.jpg',
+      title: 'Paris',
+      price: 'From $80'
+    },
+    {
+      image: '../assets/shanghai.jpg',
+      title: 'Shanghai',
+      price: '$380'
+    },
+    {
+      image: '../assets/new york.jpg',
+      title: 'New York City',
+      price: '$170'
+    }
+  ];
+
+}
